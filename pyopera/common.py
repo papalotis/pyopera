8aca12a99975f52d7954cca7140f632f8658410b
Panagiotis Tomer Karagiannis <p.karagiannis@campus.tu-berlin.de>
2021-09-17 22:02:51 +0200

    german month to int is now public

--- pyopera/common.py
+++ pyopera/common.py
@@ -6,26 +6,28 @@ from typing import Any, Mapping, Sequence, TypeVar
 
 from unidecode import unidecode
 
+GERMAN_MONTH_TO_INT = {
+    "Januar": 1,
+    "Jänner": 1,
+    "Februar": 2,
+    "März": 3,
+    "April": 4,
+    "Mai": 5,
+    "Juni": 6,
+    "Juli": 7,
+    "August": 8,
+    "September": 9,
+    "Oktober": 10,
+    "November": 11,
+    "Dezember": 12,
+}
+
 
 def austria_date_to_datetime(date_str: str) -> datetime:
     """
     Convert "Samstag, 28. August 2021" to datetime(2021, 08, 28)
     """
 
-    GERMAN_MONTH_TO_INT = {
-        "Januar": 1,
-        "Februar": 2,
-        "März": 3,
-        "April": 4,
-        "Mai": 5,
-        "Juni": 6,
-        "Juli": 7,
-        "August": 8,
-        "September": 9,
-        "Oktober": 10,
-        "November": 11,
-        "Dezember": 12,
-    }
     day_name, day, month_name, year = date_str.split(" ")
     day_int = int(day[:-1])
     month_int = GERMAN_MONTH_TO_INT[month_name]
