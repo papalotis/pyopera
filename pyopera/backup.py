e53b975999a56c45e5d9a0bd7ea8b56b70537f6f
Panagiotis Tomer Karagiannis <p.karagiannis@campus.tu-berlin.de>
2021-09-18 18:32:29 +0200

    changed backup script to only save when db has changed

--- pyopera/backup.py
+++ pyopera/backup.py
@@ -1,26 +1,45 @@
+import difflib
 import json
 from datetime import datetime
 from pathlib import Path
 
 from deta import Deta
+from icecream import ic
 
 from common import load_deta_project_key
 
 deta = Deta(load_deta_project_key())
+
 base = deta.Base("performances")
 
 
-def load_all():
+def load_all() -> list:
     return base.fetch().items
 
 
-if __name__ == "__main__":
+def do_backup() -> None:
+    print("Downloading...")
     db = load_all()
+    print("Finished downloading")
 
     directory = Path(__file__).parent.parent / "backups"
 
     directory.mkdir(parents=True, exist_ok=True)
 
-    backup_file = directory / f"{datetime.now()}_backup.json"
+    latest_backup = max(directory.iterdir(), key=lambda el: el.stat().st_ctime)
 
-    backup_file.write_text(json.dumps(db))
+    print(f"Backup file {latest_backup.name}")
+    existing_db = json.loads(latest_backup.read_text())
+
+    if db != existing_db:
+        print("Creating backup...")
+        backup_file = directory / f"{datetime.now()}_backup.json"
+        backup_file.write_text(json.dumps(db))
+    else:
+        print("Will NOT create backup")
+
+    print("Done")
+
+
+if __name__ == "__main__":
+    do_backup()
