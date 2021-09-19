8da9be4b6b74d2701046bc7fff3a197bc3ad0bbd
Panagiotis Tomer Karagiannis <p.karagiannis@campus.tu-berlin.de>
2021-09-19 17:48:07 +0200

    micro now serves static file with iframe of hosted streamlit app

--- micros/main.py
+++ micros/main.py
@@ -1,8 +1,18 @@
 import json
+from copy import deepcopy
 from datetime import datetime
-from typing import Optional, Tuple
+from ssl import create_default_context
+from typing import TYPE_CHECKING, Optional, Tuple
 
 from deta import Deta
+
+# from pyopera.common import Performance, create_key_for_visited_performance_v2
+
+if TYPE_CHECKING:
+    from pyopera.streamlit_common import DB_TYPE
+else:
+    DB_TYPE = object
+
 
 try:
     from pyopera.common import load_deta_project_key
@@ -15,7 +25,7 @@ base = deta.Base("performances")
 drive = deta.Drive("backups")
 
 
-def load_all() -> list:
+def load_all() -> DB_TYPE:
     return base.fetch().items
 
 
@@ -58,13 +68,67 @@ def do_backup() -> Tuple[bool, str]:
     return will_perform_backup, backup_filename
 
 
-from deta import app
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+
+try:
+    from deta import App
+
+    app: FastAPI = App(FastAPI())
+
+    @app.lib.run(action="backup")
+    @app.lib.cron()
+    def cron_task(event):
+        print(f"running backup")
+        did_backup, filename = do_backup()
+
+        return {"did_backup": did_backup, "filename": filename}
 
 
-@app.lib.run(action="backup")
-@app.lib.cron()
-def cron_task(event):
-    print(f"running backup")
-    did_backup, filename = do_backup()
+except ImportError:
+    app = FastAPI()
 
-    return {"did_backup": did_backup, "filename": filename}
+
+app.mount("/", StaticFiles(directory="opera", html=True), name="opera")
+
+
+# from icecream import ic
+
+
+# def remove_empty_roles_and_parts_inplace(entry: Performance) -> bool:
+#     calculate_number_of_roles = lambda entry: len(entry["cast"]) + len(
+#         entry["leading_team"]
+#     )
+
+#     old_number_of_parts_or_roles = calculate_number_of_roles(entry)
+
+#     column_keys = ("cast", "leading_team")
+#     for column_key in column_keys:
+#         role_or_part_dict = entry[column_key]
+#         for role_or_part in list(role_or_part_dict):
+#             if len(role_or_part_dict[role_or_part]) == 0:
+#                 del role_or_part_dict[role_or_part]
+
+#     return old_number_of_parts_or_roles != calculate_number_of_roles(entry)
+
+
+# # @app.lib.run(action="fix")
+# # @app.lib.cron()
+# def fix_db(event):
+
+#     print(f"running fix")
+#     # db = ic([e for e in load_all() if e["name"] == "Theodora"])
+#     db = load_all()
+#     for entry in db:
+#         new_entry = deepcopy(entry)
+#         should_update = False
+#         should_update |= remove_empty_roles_and_parts_inplace(new_entry)
+#         if should_update:
+#             new_entry["key"] = create_key_for_visited_performance_v2(new_entry)
+#             base.put(new_entry)
+
+#     # if should_update
+
+
+# fix_db(None)
